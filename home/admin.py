0d321d67a321bdf1ee79841dfeeaf638aa11bcd5
slojararshavin <slojararshavin@gitlab.com>
2022-07-21 22:21:03 +0100

    admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Hostel, Profile, Department
 
-# Register your models here.
+
+class ProfileModelAdmin(admin.ModelAdmin):
+    list_display = ["user", "phone_number", "account_type"]
+
+
+admin.site.register(Department)
+admin.site.register(Hostel)
+admin.site.register(Profile, ProfileModelAdmin)
