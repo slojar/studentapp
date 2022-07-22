b70c85dfdf807c99017f9d037ed85f4e3624bdfe
slojararshavin <slojararshavin@gitlab.com>
2022-07-22 21:52:07 +0100

    database link to heroku

--- studentapp/settings.py
+++ studentapp/settings.py
@@ -12,6 +12,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
+    "https://studenthostelapp.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:80",
     "http://localhost",
