95634fe013f66022af24609ae60431925018bace
slojararshavin <slojararshavin@gitlab.com>
2022-07-22 09:54:44 +0100

    database link to heroku

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.RegisterAPIView.as_view(), name="register"),
     path('hostels/', views.HostelListCreateAPIView.as_view(), name="department"),
     path('student/', views.FetchStudentAPIView.as_view(), name="student"),
+    path('login/', views.LoginAPIView.as_view(), name="login"),
 ]
 
